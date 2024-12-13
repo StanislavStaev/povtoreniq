b82a10ce6bff6cb861541a9bc49931c00ebc3354
StanislavStaev <stanislavstaev09@gmail.com>
2024-12-13 09:08:08 +0200

    Add files via upload

---
+++ sybirane na 4isla/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sybirane_na_4isla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var i = 1;
+            var s = 1;
+            var a = 1;
+            do
+            {    
+             s = n /1000;
+             i = n %1000/100;
+             a= n %10;
+
+            }
+            while (n < 0);
+            Console.WriteLine("sum of digits:{0}",i);
+            Console.WriteLine("sum of digits:{0}",s);
+            Console.WriteLine("sum of digits:{0}",a);
+        }
+    }
+}
