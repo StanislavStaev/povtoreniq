8e8f22ad7ee1fb6e87d3b2a158ae407d3ea62147
StanislavStaev <stanislavstaev09@gmail.com>
2024-12-13 09:04:51 +0200

    Add files via upload

---
+++ faktoriel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktoriel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("vyvedi 4islo  ");
+            int P = 1;
+            int n = int .Parse(Console.ReadLine()); 
+            do
+            {
+                P = P * n;
+                n--;
+            }
+            while (n >1);
+            Console.WriteLine("P="+P);
+        }
+    }
+}
